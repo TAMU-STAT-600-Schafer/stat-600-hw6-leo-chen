daac7e1b916bcab65e15833dcb6e6149b525dca7
Leo Zexian Wang <leowang@tamu.edu>
2024-11-02 17:20:32 -0500

    performed more tests on the function LRMultiClass_c in Tests.R

--- src/Tests.R
+++ src/Tests.R
@@ -26,48 +26,37 @@ all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
 
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 500, eta = 0.1, lambda = 1) # more iterations
-out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 500, eta = 0.1, lambda = 1)
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 500, eta = 0.1, lambda = 1)
 all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
 
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 1) # larger eta
-out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 1)
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.5, lambda = 1)
 all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
 
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.01, lambda = 1) # smaller eta
-out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.01, lambda = 1)
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.01, lambda = 1)
 all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
 
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 5) # larger lambda
-out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 5)
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.5, lambda = 5)
 all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
 
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 0.5) # smaller lambda
-out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 0.5)
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.5, lambda = 0.5)
 all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
 
-# compatibility checks to see if error messages will be returned appropriately
-out = LRMultiClass(matrix(rnorm(17*20), 17), Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
-out = LRMultiClass(X, Y, matrix(rnorm(5*20), 5), Yt, numIter = 50, eta = 0.1, lambda = 1)
-out = LRMultiClass(X, c(0, 1, 2, 3), Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
-out = LRMultiClass(X, Y, Xt, c(1, 2), numIter = 50, eta = 0.1, lambda = 1)
-out = LRMultiClass(X, Y, cbind(1, Xt), Yt, numIter = 50, eta = 0.1, lambda = 1)
-out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = -0.1, lambda = 1)
-out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = -1)
-out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = matrix(1, 20, 5))
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
-out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = matrix(10, 20, 5))
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
-out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = matrix(0, 20, 2))
-out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = matrix(0, 2, 5))
+# comparing speeds of R and C++ functions
+library(microbenchmark)
+print(microbenchmark(
+  LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1),
+  LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.1, lambda = 1),
+  times = 20
+))
 
 # 2nd test case
 Y = rbinom(200, size = 10, prob = 0.5) - 1
@@ -77,36 +66,44 @@ Xt = matrix(rbinom(30*99, 10, 0.5), 30)
 X = cbind(1, X)
 Xt = cbind(1, Xt)
 K = max(Y) + 1
-beta_init = matrix(0, 20, K)
+beta_init = matrix(0, 100, K)
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
 out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.1, lambda = 1)
 all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
 
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 500, eta = 0.1, lambda = 1) # more iterations
-out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 500, eta = 0.1, lambda = 1)
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 500, eta = 0.1, lambda = 1)
 all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
 
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 1) # larger eta
-out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 1)
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.5, lambda = 1)
 all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
 
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.01, lambda = 1) # smaller eta
-out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.01, lambda = 1)
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.01, lambda = 1)
 all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
 
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 5) # larger lambda
-out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 5)
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.5, lambda = 5)
 all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
 
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 0.5) # smaller lambda
-out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 0.5)
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.5, lambda = 0.5)
 all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
+
+# comparing speeds of R and C++ functions
+library(microbenchmark)
+print(microbenchmark(
+  LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1),
+  LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.1, lambda = 1),
+  times = 20
+))
 
 # tests relating to the letter example
 letter_train <- read.table("Data/letter-train.txt", header = F, colClasses = "numeric")
@@ -117,8 +114,45 @@ Yt <- letter_test[, 1]
 Xt <- as.matrix(letter_test[, -1])
 X = cbind(1, X)
 Xt = cbind(1, Xt)
-out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = matrix(1, ncol(X), length(unique(Y))))
-out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = matrix(1, ncol(X), length(unique(Y))))
+K = max(Y) + 1
+beta_init = matrix(0, 17, K)
+out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.1, lambda = 1)
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
+out = LRMultiClass(X, Y, Xt, Yt, numIter = 500, eta = 0.1, lambda = 1) # more iterations
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 500, eta = 0.1, lambda = 1) # more iterations
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
+out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 1) # larger eta
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.5, lambda = 1) # larger eta
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
+out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.01, lambda = 1) # smaller eta
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.01, lambda = 1) # smaller eta
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
+out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 5) # larger lambda
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.5, lambda = 5) # larger lambda
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
+out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 0.5) # smaller lambda
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.5, lambda = 0.5) # smaller lambda
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
+# comparing speeds of R and C++ functions
+library(microbenchmark)
+print(microbenchmark(
+  LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1),
+  LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.1, lambda = 1),
+  times = 20
+))
 
 
 # ====================== Tests for MyKmeans ======================
