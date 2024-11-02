d3a77c8c82583d70e930d9f82eb3fd09eb9bf627
Leo Zexian Wang <leowang@tamu.edu>
2024-11-02 17:05:07 -0500

    performed more tests on the function LRMultiClass_c in Tests.R

--- src/Tests.R
+++ src/Tests.R
@@ -24,29 +24,31 @@ out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
 out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.1, lambda = 1)
 all.equal(out$beta, out_c$beta)
 all.equal(out$objective, out_c$objective)
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 500, eta = 0.1, lambda = 1) # more iterations
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 500, eta = 0.1, lambda = 1)
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 1) # larger eta
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 1)
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.01, lambda = 1) # smaller eta
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.01, lambda = 1)
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 5) # larger lambda
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 5)
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 0.5) # smaller lambda
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 0.5)
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
 
 # compatibility checks to see if error messages will be returned appropriately
 out = LRMultiClass(matrix(rnorm(17*20), 17), Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
@@ -74,30 +76,37 @@ Yt = rbinom(30, size = 10, prob = 0.5) - 1
 Xt = matrix(rbinom(30*99, 10, 0.5), 30)
 X = cbind(1, X)
 Xt = cbind(1, Xt)
+K = max(Y) + 1
+beta_init = matrix(0, 20, K)
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1)
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+out_c = LRMultiClass_c(X, Y, beta_init, numIter = 50, eta = 0.1, lambda = 1)
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 500, eta = 0.1, lambda = 1) # more iterations
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 500, eta = 0.1, lambda = 1)
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 1) # larger eta
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 1)
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.01, lambda = 1) # smaller eta
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.01, lambda = 1)
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 5) # larger lambda
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 5)
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
+
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 0.5) # smaller lambda
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.5, lambda = 0.5)
+all.equal(out$beta, out_c$beta)
+all.equal(out$objective, out_c$objective)
 
 # tests relating to the letter example
 letter_train <- read.table("Data/letter-train.txt", header = F, colClasses = "numeric")
@@ -109,9 +118,8 @@ Xt <- as.matrix(letter_test[, -1])
 X = cbind(1, X)
 Xt = cbind(1, Xt)
 out = LRMultiClass(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = matrix(1, ncol(X), length(unique(Y))))
-plot(out$objective, type = 'o', xlab = "number of iterations", ylab = "objective values")
-plot(out$error_train, type = 'o', xlab = "number of iterations", ylab = "training error")
-plot(out$error_test, type = 'o')
+out_c = LRMultiClass_c(X, Y, Xt, Yt, numIter = 50, eta = 0.1, lambda = 1, beta_init = matrix(1, ncol(X), length(unique(Y))))
+
 
 # ====================== Tests for MyKmeans ======================
 # Test Case 1: Two Distinct Clusters in 2D
